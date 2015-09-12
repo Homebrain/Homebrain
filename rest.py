3735a44c5640d3ce76c1f2e2e5cad84736e5f4ee
johan-bjareholt <johanthepro@gmail.com>
2015-09-12 21:36:56 +0200

    Added web interface

--- rest.py
+++ rest.py
@@ -1,8 +1,44 @@
 from flask import Flask, Response, request, json
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='', static_folder='./site')
 
-@app.route('/api/0/test')
+#
+#	Static Files
+#
+
+@app.route("/")
+@app.route("/<_>")
+@app.route("/<_>/<__>")
+@app.route("/<_>/<__>/<___>")
+def index(**_):
+	return app.send_static_file("index.html")
+
+@app.route("/scripts/<file>")
+def scripts(file):
+    return app.send_static_file("scripts/" + file)
+
+
+@app.route("/templates/<file>")
+def templates(file):
+    return app.send_static_file("templates/" + file)
+
+#
+#	REST API
+#
+
+@app.route("/api/v0/nodes")
+def get_nodes():
+    agents = []
+    #return json.dumps(agents)
+    return ""
+
+@app.route("/api/v0/agents")
+def get_agents():
+    agents = {}
+    #return json.dumps(agents)
+    return ""
+
+@app.route('/api/v0/test')
 def test_endpoint():
     data = {"msg": "Hello World!"}
     payload = json.dumps(data)
---
+++ site/scripts/main.js
@@ -0,0 +1,62 @@
+app = angular.module('HomeBrain', ["ngResource", "ngRoute"]);
+
+
+app.config(function($routeProvider, $locationProvider) {
+    $routeProvider.when("/", {
+        templateUrl: "templates/dashboard.html",
+        controller: "HomeCtrl"
+    });
+
+    $routeProvider.when("/nodes", {
+        templateUrl: "templates/nodes.html",
+        controller: "NodesCtrl"
+    });
+
+    $routeProvider.when("/nodes/:id", {
+        templateUrl: "templates/node.html",
+        controller: "NodeCtrl"
+    });
+
+    $routeProvider.when("/agents", {
+        templateUrl: "templates/agents.html",
+        controller: "AgentsCtrl"
+    });
+
+    $routeProvider.when("/agents/:id", {
+        templateUrl: "templates/agent.html",
+        controller: "AgentCtrl"
+    });
+
+    $locationProvider.html5Mode(true);
+});
+
+app.controller("MainCtrl", function($scope, $route, $routeParams, $location) {
+    $scope.$route = $route;
+    $scope.$routeParams = $routeParams;
+    $scope.$location = $location;
+});
+
+app.controller("HomeCtrl", function($scope) {
+});
+
+function capitalize(s) {
+    return s.substr(0, 1).toUpperCase() + s.substr(1);
+}
+
+app.controller("BreadcrumbCtrl", function($scope, $location) {
+});
+
+app.controller("AgentListCtrl", function($scope, $resource, $interval) {
+});
+
+app.controller("NodesCtrl", function($scope, $resource, $routeParams) {
+});
+
+app.controller("NodeCtrl", function($scope, $resource, $routeParams) {
+});
+
+app.controller("AgentCtrl", function($scope, $resource, $routeParams) {
+});
+
+app.controller("AgentsCtrl", function($scope, $resource, $routeParams) {
+});
