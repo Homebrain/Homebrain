31a6223f10a23e4dd3ccdf95a0785e275516fab8
Erik Bjäreholt <erik.bjareholt@gmail.com>
2015-09-14 13:27:01 +0200

    Merged in changes to dispatcher, commented out my implementation for review

--- homebrain/dispatcher.py
+++ homebrain/dispatcher.py
@@ -2,7 +2,7 @@ from .util.event_thread import EventThread
 
 class Dispatcher(EventThread):
     def __init__(self, agentmanager):
-        super(Dispatcher, self).__init__()
+        EventThread.__init__(self)
         self._agentmanager = agentmanager
 
     def run(self):
@@ -12,3 +12,25 @@ class Dispatcher(EventThread):
     def process(self, event):
         for subber in self._agentmanager.get_subscribers(event["type"]):
             subber.post(event)
+
+"""
+Not sure which code to use, commenting this out for others to review.
+
+    bindings = {}
+
+    def bind(self, agent, selector):
+        if selector not in self.bindings.keys() or not self.bindings[selector]:
+            self.bindings[selector] = set()
+
+        self.bindings[selector].add(agent)
+
+    def _query_selector(self, selector):
+        "Returns all the agents that match the selector"
+        return self.bindings[selector]
+
+    def process(self, msg):
+        handlers = _get_handlers(event["type"])
+        for handler in handlers:
+            handler.add_message(msg)
+
+"""
