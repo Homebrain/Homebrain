ace14402cf72bb176d36ae79927ecbf1b6f42eea
johan-bjareholt <johanthepro@gmail.com>
2015-09-19 20:00:49 +0200

    Added IDFilter and removed buttonlistener

---
+++ homebrain/agents/idfilter/IDFilter.py
@@ -0,0 +1,16 @@
+from homebrain import Agent, Event, Dispatcher
+
+
+class IDFilter(Agent):
+    """Listens to a certain trigger event, and emits a target event once the specified count has been reached."""
+    def __init__(self, ID, target=None):
+        Agent.__init__(self)
+        self.target = target if target is not None else self.identifier
+        self.id = ID
+
+    def run(self):
+        while True:
+            event = self.next_event()
+            print(event)
+            if "id" in event and event["id"] == self.id:
+                Dispatcher().post(Event(**{"type":self.target, "data": event["data"]}))
