bb916cb5716a3df4a3a9246830e2370fce4e58b0
Erik Bjäreholt <erik.bjareholt@gmail.com>
2015-09-19 20:57:15 +0200

    Changed tabs to spaces, made say() method not block waiting for process to exit, renamed things

--- homebrain/clients/speech.py
+++ homebrain/clients/speech.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import os
+import shlex
 import subprocess
 
 from flask import Flask, request
@@ -8,38 +9,44 @@ import json
 import requests
 app = Flask(__name__)
 
+ERROR_MSG_ESPEAK_MISSING = """Could not find espeak
+Or please install it or if it is already installed, please add its directory to your environment variable"""
+
+
 def say(msg, language="english"):
-	print(os.system("espeak -v {} \"{}\"".format(language, msg)))
+    print("Saying: " + msg)
+    command = shlex.split("espeak -v {} \"{}\"".format(language, msg))
+    subprocess.Popen(command)
 
-def isEspeakInstalled():
-	out = open("/dev/null")
-	statuscode = subprocess.call(["which", "espeak"], stdout=out, stderr=out)
-	if statuscode > 0:
-		return False
-	else:
-		return True
 
-def main():
-	say("testing")
+def is_espeak_installed():
+    out = open("/dev/null")
+    statuscode = subprocess.call(["which", "espeak"], stdout=out, stderr=out)
+    if statuscode > 0:
+        return False
+    else:
+        return True
+
 
 @app.route("/", methods=["POST"])
 def post_event():
-	event = json.loads(request.json)
-	data = event["data"]
-	print(data)
-	# Language
-	language = "english"
-	if "language" in data:
-		language = data["language"]
-	# Msg
-	msg = event["data"]["msg"]
-	# Talk
-	say(msg, language)
-	return ""
+    event = json.loads(request.json)
+    data = event["data"]
+    print(data)
+    # Language
+    language = "english"
+    if "language" in data:
+        language = data["language"]
+    # Msg
+    msg = event["data"]["msg"]
+    # Talk
+    say(msg, language)
+    return ""
 
 if __name__ == "__main__":
-	if isEspeakInstalled():
-		app.run(debug=True, port=9092)
-	else:
-		print("Could not find espeak\n" +
-			  "If it is not installed, please install it and if it is installed, please add its directory to your environment variable")
+    if is_espeak_installed():
+        app.run(debug=True, port=9092)
+    else:
+        print("\n" +
+              "" +
+              "")
