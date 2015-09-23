66d498900364688564c9da1efa9c2321c9e6ab3a
Valthor <vlthrh@gmail.com>
2015-09-23 23:20:35 +0200

    added mock tests for Agent logging decorators

--- tests/test_agent.py
+++ tests/test_agent.py
@@ -1,8 +1,9 @@
-from homebrain import Agent, Event, Dispatcher
-from homebrain import Agent, AgentManager, Dispatcher, Event
+from homebrain import AgentManager, Dispatcher
+from homebrain.base import Agent, Event
 import unittest
 import threading
 import time
+from unittest.mock import patch
 
 WAIT_TIME = 0.02  # time to sleep while waiting for a thread to react
 
@@ -13,8 +14,12 @@ class MockAgent(Agent):
         self.events = []
 
     @Agent.stop_on_shutdown_event
+    @Agent.log_exceptions
+    @Agent.log_events
     def handle_event(self, event):
         self.events.append(event)
+        if event["type"] == "test_exception":
+            raise RuntimeError("This exception is unexpected and not caught by handle_event itself")
 
     def cleanup(self):
         pass
@@ -43,5 +48,28 @@ class AgentTest(unittest.TestCase):
         time.sleep(WAIT_TIME)
         self.assertEqual(1, len(self.mock_agent.events))
 
+    @patch("homebrain.base.logging")
+    def test_logging_decorators(self, mock_logging):
+        """The logging decorators should yield one logging call per event, plus one per exception."""
+        self.assertEqual(0, total_debug_calls(mock_logging))
+        self.mock_agent.post(Event(type="button", data={}))
+        self.mock_agent.post(Event(type="button", data={}))
+        self.mock_agent.post(Event(type="button", data={}))
+        time.sleep(WAIT_TIME)
+        self.assertEqual(3, total_debug_calls(mock_logging))
+        self.mock_agent.post(Event(type="test_exception", data={}))
+        time.sleep(WAIT_TIME)
+        self.assertEqual(5, total_debug_calls(mock_logging))
+        self.assertEqual(1, mock_logging.exception.call_count)
+
     def tearDown(self):
         self.mock_agent.stop()
+
+def total_debug_calls(mock_logging):
+    total_debug_calls = mock_logging.debug.call_count +\
+                        mock_logging.info.call_count +\
+                        mock_logging.warn.call_count +\
+                        mock_logging.error.call_count +\
+                        mock_logging.exception.call_count +\
+                        mock_logging.critical.call_count
+    return total_debug_calls
