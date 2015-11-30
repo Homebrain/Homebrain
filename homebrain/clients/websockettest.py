8c63f72798f60f7363a4fd4646041ad9dd80d451
Johan Bjäreholt <johanthepro@gmail.com>
2015-11-30 17:49:12 +0100

    Fixed websocket client to the new library api

--- homebrain/clients/websockettest.py
+++ homebrain/clients/websockettest.py
@@ -7,28 +7,17 @@ import json
 def on_message(ws, message):
     print(message)
 
-
 def on_error(ws, error):
     print("WSERROR: " + error)
 
-
 def on_close(ws):
     print("closing")
-
 
 def on_open(ws):
     msg = {"type": "log", "data": {"level": "info", "msg": "Hello Homebrain! I am a websocket client!"}}
     ws.send(json.dumps(msg))
     msg = {"type": "subscribe", "data": "log"}
     ws.send(json.dumps(msg))
-    try:
-        while True:
-            log_msg = ws.recv()
-            print(log_msg)
-    except KeyboardInterrupt:
-        pass
-    ws.close()
-
 
 if __name__ == "__main__":
     ws = websocket.WebSocketApp("ws://127.0.0.1:20445/",
