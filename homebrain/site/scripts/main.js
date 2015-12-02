be45dd437e22e3a63475ab7557e39228919607b3
Johan Bjäreholt <johanthepro@gmail.com>
2015-12-02 10:10:40 +0100

    Fixed dashboard and single agent view reporting all agents dead

--- homebrain/site/scripts/main.js
+++ homebrain/site/scripts/main.js
@@ -88,7 +88,7 @@ app.controller("HomeCtrl", function($scope, $resource) {
         var c = 0
         for (node in $scope.agents) {
             // TODO: Don't represent node status as a string when the string only represents a boolean
-            if ($scope.agents[node].status == "True")
+            if ($scope.agents[node].status == true)
                 c++;
         }
         $scope.agentsrunning = c;
