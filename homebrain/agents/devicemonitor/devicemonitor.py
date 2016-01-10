a947c5f8cfb9c66d81c60c855fc342e29e8ae919
Johan Bjäreholt <johanthepro@gmail.com>
2016-01-10 19:19:53 +0100

    Commented and cleaned up DeviceMonitor

--- homebrain/agents/devicemonitor/devicemonitor.py
+++ homebrain/agents/devicemonitor/devicemonitor.py
@@ -11,11 +11,13 @@ from homebrain import Agent, Event, Dispatcher
 class DeviceMonitor(Agent):
     def __init__(self, target=None, netmask=24):
         super(DeviceMonitor, self).__init__()
+        # Initialize values
         self.target = target if target is not None else self.identifier
         self.dispatcher = Dispatcher()
         self.threadpool = ThreadPool(5)
         self.updateinterval = 30
-
+        self._known_devices = {}
+        # Fetch network device IPs
         default_gateway = netifaces.gateways()['default']
         if (default_gateway):
             (gateway_ip, interface) = default_gateway[netifaces.AF_INET]
@@ -23,42 +25,49 @@ class DeviceMonitor(Agent):
         else:
             logging.error("DeviceMonitor couldn't start, no default gateway available")
             self.stop()
-        self._known_devices = {}
-        self._connected_devices = set()
 
     def run(self):
         start_time = time.time()
-        while self._enabled:
-            current_devices = self._get_active_devices()
-            connected_devices = current_devices - self._connected_devices
-            disconnected_devices = self._connected_devices - current_devices
+        available_devices = set()
 
-            for (ip, hostname) in connected_devices:
+        while self._enabled:
+            # Get current local network devices
+            current_devices = self._get_active_devices()
+            # Filter out new and lost devices
+            new_devices = current_devices - available_devices
+            lost_devices = available_devices - current_devices
+
+            for (ip, hostname) in new_devices:
                 data = {'data': {'status': 'connected', 'hostname': hostname}}
                 self.dispatcher.put_event(Event(type='device_connection', data=data))
                 self._known_devices[hostname] = {'ip': ip, 'hostname': hostname, 'status': True, 'ports':[]}
-                #logging.info('device_connection: '+str(self._known_devices[hostname]))
 
-            for (ip, hostname) in disconnected_devices:
+            for (ip, hostname) in lost_devices:
                 data = {'data': {'status': 'disconnected', 'hostname': hostname}}
                 self.dispatcher.put_event(Event(type='device_connection', data=data))
                 self._known_devices[hostname]['status'] = False
-                #logging.info('device_connection: '+str(data))
 
-            self._connected_devices = current_devices
+            # Update available devices
+            available_devices = current_devices
+
+            # Check all devices open homebrain ports
             openports = self._get_open_ports()
             for item in openports:
+                # Get past values
                 device = self._known_devices[item[0]]
                 currentports = item[1]
                 prevports = set(device["ports"])
+                # Filter out new and lost ports
                 newports = set(currentports)-prevports
-                oldports = set(prevports)-set(currentports)
+                lostports = set(prevports)-set(currentports)
                 if newports:
-                    logging.info("New homebrain port from '{}' available: {}".format(device["hostname"], str(newports)))
-                if oldports:
-                    logging.info("Homebrain port from '{}' is now unavailable: {}".format(device["hostname"], str(oldports)))
+                    logging.info("New homebrain node(s) from '{}' available: {}".format(device["hostname"], str(newports)))
+                if lostports:
+                    logging.info("Homebrain node(s) from '{}' is now unavailable: {}".format(device["hostname"], str(lostports)))
+                # Update to new ports
                 device["ports"] = currentports
 
+            # Wait for the next update interval
             while start_time + self.updateinterval > time.time():
                 time.sleep(1)
             start_time = time.time()
@@ -78,6 +87,7 @@ class DeviceMonitor(Agent):
         return self._known_devices
 
 def _is_active(ip):
+    # Ping device to see if it is alive
     try:
         (hostname, aliaslist, ipaddrlist) = socket.gethostbyaddr(ip)
         response = os.system("ping -c 1 -t 1 " + ip + " > /dev/null 2>&1")
@@ -88,7 +98,7 @@ def _is_active(ip):
     return None
 
 def _ports_open(ip):
-    #print(ip)
+    # Get all open HomeBrain ports from devices
     ports = []
     for port in range(5602, 5610):
         if _port_is_open(ip, port):
@@ -96,6 +106,7 @@ def _ports_open(ip):
     return (ip, ports)
 
 def _port_is_open(ip, port):
+    # Check open port on device
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.settimeout(0.5)
     result = 1
