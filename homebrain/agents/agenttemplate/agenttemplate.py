b8ccbe40ffecb17e8f05c7ad245d8af8eff05093
Johan Bjäreholt <johanthepro@gmail.com>
2016-03-01 19:42:19 +0100

    Renamed agenttemplate and fixed error message

--- homebrain/agents/agenttemplate/agenttemplate.py
+++ homebrain/agents/agenttemplate/agenttemplate.py
@@ -1,16 +1,16 @@
 from homebrain import Agent, Dispatcher
 from homebrain.core.decorators import stop_on_shutdown_event
+import logging
 
-
-class TemplateAgent(Agent):
+class AgentTemplate(Agent):
     def __init__(self, target=None):
-        super(TemplateAgent, self).__init__()
+        super(AgentTemplate, self).__init__()
         self.target = target if target is not None else self.identifier
 
         # Only bind manually if you are an endpoint agent. Otherwise, use Dispatcher.chain at config time.
-        Dispatcher().bind(self, "exampletype1")
+        #Dispatcher().bind(self, "exampletype1")
 
     @stop_on_shutdown_event
     def handle_event(self, event):
         # Handle incoming events
-        pass
+        logging.log(event)
